fc260310b8a280db70dd7fad41d238d657728308
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-05-21 09:54:09 -0600

    Added UMAPS

--- Tutorial.r
+++ Tutorial.r
@@ -47,3 +47,33 @@ pbmc <- JackStraw(pbmc, num.replicate = 100)
 pbmc <- ScoreJackStraw(pbmc, dims = 1:20)
 JackStrawPlot(pbmc, dims = 1:15)
 ElbowPlot(pbmc)
+
+#Cluster the cells
+pbmc <- FindNeighbors(pbmc, dims = 1:10) #Construct KNN graph and refine edge weights between two cells based on the shared overlap of local neighbors using Jaccard Index
+pbmc <- FindClusters(pbmc, resolution = 0.5) #Run Louvain algorithm to cluster cells together with resolution parameter of 0.5. Useful for good returns of single cell datasets around 3000 cells
+head(Idents(pbmc), 5) #Look at cluster IDs of the first five cells.
+
+#Run non-linear dimensional reduction (UMAP/tSNE)
+pbmc <- RunUMAP(pbmc, dims = 1:10) #RunUMAP vis tool on pbmc with dims of 1:10
+DimPlot(pbmc, reduction = "umap") #Create DimPlot to visualize individual clusters
+
+#Finding differentially expressed features (cluster biomarkers)
+cluster1.markers <- FindMarkers(pbmc, ident.1 = 1, min.pct = 0.25) #Find all markers for differential expression using a min percentage in either two groups of cells
+head(cluster1.markers, n = 5) #Check first five markers of cluster 1
+cluster5.markers <- FindMarkers(pbmc, ident.1 = 5, ident.2 = c(0, 3), min.pct = 0.25) #Find all markers distinguishing cluster 5 from clusters 0 and 3
+head(cluster5.markers, n = 5) #Check first five markers of cluster 5
+pbmc.markers <- FindAllMarkers(pbmc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25) #Find markers for every cluster compared to all remaining cells report only the positve ones
+pbmc.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
+cluster1.markers <- FindMarkers(pbmc, ident.1 = 0, logfc.threshold = 0.25, test.use = "roc", only.pos = TRUE) #Conduct ROC test on cluster 1. Returns classification power for any individual marker
+VlnPlot(pbmc, features = c("MS4A1", "CD79A")) #Construct violin plot to show expression probability distributions across clusters
+VlnPlot(pbmc, features = c("NKG7", "PF4"), slot = "counts", log = TRUE) #Plot raw counts as well for two different cells
+FeaturePlot(pbmc, features = c("MS4A1", "GNLY", "CD3E", "CD14", "FCER1A", "FCGR3A", "LYZ", "PPBP", "CD8A")) #Create feature plot of nine cell types for UMAPs
+top10 <- pbmc.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC) #Create top 10 markers for each cluster
+DoHeatmap(pbmc, features = top10$gene) + NoLegend() #Create heat map of top 10 markers
+
+#Assigning cell type identity to clusters
+new.cluster.ids <- c("Naive CD4 T", "Memory CD4 T", "CD14+ Mono", "B", "CD8 T", "FCGR3A+ Mono", "NK", "DC", "Platelet") #Assign cell types to clusters
+names(new.cluster.ids) <- levels(pbmc)
+pbmc <- RenameIdents(pbmc, new.cluster.ids)
+DimPlot(pbmc, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+head(Idents(pbmc), 5) # Look at cluster IDs of the first 5 cells
